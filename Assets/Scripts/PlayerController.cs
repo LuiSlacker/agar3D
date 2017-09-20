8a9124c7b8eea5fa7a0d4f644ceaeb8be2688a70
Petulant Slacker <l.goohsen@gmail.com>
2017-09-20 15:40:09 -0500

    added enemies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,11 +15,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void Update () {
-		
 	}
 
 	void FixedUpdate() {
 		rb.AddForce(Input.GetAxis("Vertical") * target.transform.forward * speed);
 		rb.AddForce(Input.GetAxis("Horizontal") * target.transform.right * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("enemy")) {
+			other.gameObject.SetActive (false);
+			transform.localScale = Vector3.Scale(transform.localScale, new Vector3(1.1f,1.1f,1.1f));
+		}
 	}
 }
