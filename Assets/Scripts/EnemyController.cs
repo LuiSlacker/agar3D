9957228cbbd8268d01992f6f2d822ba2006ba0df
Petulant Slacker <l.goohsen@gmail.com>
2017-11-08 15:38:26 -0500

    moved Intervall function to Start()

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,22 +10,30 @@ public class EnemyController : MonoBehaviour {
 	private int moveSpeed = 2;
 
 
+	private GameObject objectToMoveTo;
+	private Quaternion randomRotation;
+
+
 	void Start () {
 		target = GameObject.FindWithTag("Player").transform; //target the player
+		InvokeRepeating("findAndSetClosestSmallerGameObject", 0.0f, 2.0f);
 	}
 
 	void Update() {
-		//rotate to look at the player
-		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), rotationSpeed * Time.deltaTime);
+		// set a new object to move to every 2 seconds
 
-		//move towards the player
-		bool isPlayerSmallerThanEnemy = target.lossyScale.x < transform.GetChild(0).lossyScale.x;
-		if (isPlayerSmallerThanEnemy) {
-			transform.position += transform.forward * moveSpeed * Time.deltaTime;
-  		}
+
+		//rotate to look at the object
+		Debug.Log(this.objectToMoveTo);
+		transform.rotation = (this.objectToMoveTo != null)
+			? Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (this.objectToMoveTo.transform.position - transform.position), rotationSpeed * Time.deltaTime)
+			: this.randomRotation;
+
+		//move towards the object
+		transform.position += transform.forward * moveSpeed * Time.deltaTime;
 	}
 
-//	void OnTriggerEnter(Collider other) {
+	void OnTriggerEnter(Collider other) {
 //		if (other.gameObject.CompareTag ("foodWrapper") || other.gameObject.CompareTag ("enemyWrapper")) {
 //			Transform child = other.gameObject.transform.GetChild(0);
 //			bool isFoodSmallerThanPlayer = child.gameObject.transform.lossyScale.x < transform.lossyScale.x;
@@ -49,7 +57,28 @@ public class EnemyController : MonoBehaviour {
 //				gameOver ();
 //			}
 //		}
-//	}
+	}
+
+	private void findAndSetClosestSmallerGameObject() {
+		float minDistance = float.MaxValue;
+		GameObject closestGameObject = null;
+		Collider[] colliders = Physics.OverlapSphere(transform.position, 15);
+		foreach (Collider collider in colliders) {
+			float distanceToOtherObject = getDistanceToOtherObject (collider);
+			if(collider.transform.lossyScale.x < this.transform.GetChild(0).lossyScale.x
+			&& distanceToOtherObject < minDistance
+				&& collider.gameObject != this.gameObject) {
+				minDistance = distanceToOtherObject;
+				closestGameObject = collider.gameObject;
+			}
+		}
+		this.objectToMoveTo = closestGameObject;
+		this.randomRotation = Random.rotation;
+	}
+
+	private float getDistanceToOtherObject(Collider collider) {
+		return Vector3.Distance (collider.transform.position, this.transform.GetChild (0).position);
+	}
 
 	void gameOver() {
 		target.gameObject.SetActive (false);
